b3279b54d510dee80451224fb43fea73ee694249
sujeet <sujeet901626@gmail.com>
2025-10-11 07:56:30 +0530

    props done

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-5'>Tailwind test</h1>
+      <Card username="sujeet" btnText="click me" view="500K"/>
+    </>
+  )
+}
+
+export default App
